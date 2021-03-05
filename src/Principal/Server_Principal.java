71827a59c612bc6b5c7dc24fdf404c1fa00224d0
omarALess12 <78387308+omarALess12@users.noreply.github.com>
2021-03-05 15:36:32 -0600

    Update Server_Principal.java

--- src/Principal/Server_Principal.java
+++ src/Principal/Server_Principal.java
@@ -15,7 +15,7 @@ public class Server_Principal {
     public static void main(String[] args) {       
         try{        
 	     System.out.println("Iniciando el servidor XML-RPC...");	     
-	     WebServer server = new WebServer(5005);
+	     WebServer server = new WebServer(80);
 	     Control_Server op = new Control_Server();
 	     server.addHandler("miServidor", op);
 	     server.start();
